d83f8caf0f9f8491b8814c647bf7da0640ffe8ec
Sandeepbay <sandeepbay2002@gmail.com>
2023-07-22 16:47:42 +0530

    Created Database
    Created and Connected Database successfully using MongoDB.
    Created the necessary files to make the Application.

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,8 @@
 import express from 'express'
+import mongoose from 'mongoose'
 const app = express();
 
-app.use('/api' , (req,res,next) => {
-    res.send("Hello World")
-})
-
-app.listen(3000 , () => {
-    console.log("Server 300 Started")
-})
+mongoose.connect('mongodb+srv://Sandeepbay:joFSLlFluTpUaJhX@cluster0.aieufnm.mongodb.net/BlogApp?retryWrites=true&w=majority')
+.then(() => app.listen(3000))
+.then(() => console.log("Connected the database and Server 300 Started"))
+.catch((err) => console.log(err))
